1fe187bfedec3b723afe9465f38dfc9b8e659641
Dmytrii Basov <dimon4a2009@gmail.com>
2021-11-04 10:58:19 +0200

    Redefined default User model & manager. Added simple SignUp View

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 
 
 # Create your models here.
 class User(AbstractUser):
+
+    username = None
 
     birth_date = models.DateField(
         null=True,
@@ -11,8 +15,16 @@ class User(AbstractUser):
     )
 
     email = models.EmailField(
+        _('email address'),
         unique=True,
         blank=False,
         null=False,
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    object = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+
+from .models import User
+from .serializers import SignUpSerializer
+
+
+class UsersList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+
+
+
