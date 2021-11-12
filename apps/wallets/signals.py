195bfa5638d51445194a4c70ff3811ea5f7b63d1
Dmytrii Basov <dimon4a2009@gmail.com>
2021-11-12 16:50:05 +0200

    minor wallets updates

--- apps/wallets/signals.py
+++ apps/wallets/signals.py
@@ -10,15 +10,3 @@ from ..users.models import User
 def create_wallet(sender, instance, created, **kwargs):
     if created:
         Wallet.objects.create(id=instance.id, user=instance)  # TEMPORARY HARDCODED wallet_id == user_id!! TO CHANGE
-
-
-# @receiver(post_save, sender=Transaction)
-# def update_balance(sender, instance, created, **kwargs):
-#     if created:
-#         transaction = instance
-#         user = transaction.user
-#         wallet = transaction.wallet
-#         if transaction.FILL:
-#             balance = Transaction.objects.filter(user=user).aggregate((Sum('value')))
-#             print(balance['value__sum'])
-#             Wallet.objects.filter(id=user.id).update(balance=balance['value__sum'])
