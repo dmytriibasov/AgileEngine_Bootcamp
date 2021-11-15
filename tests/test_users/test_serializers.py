d38abaa039cac623e6a2f946ad6fa975d31f24d0
Dmytrii Basov <dimon4a2009@gmail.com>
2021-11-15 20:17:22 +0200

    pytests for users

--- tests/test_users/test_serializers.py
+++ tests/test_users/test_serializers.py
@@ -1,13 +1,17 @@
 import pytest
-from AgileEngine_Bootcamp.apps.users.serializers import SignUpSerializer
+from apps.users.serializers import SignUpSerializer
 
 
 @pytest.mark.django_db
-def test_signup_serializer():
-    data = {'email': 'custom@email.com',
-            'password': 'ProAdmin888',
-            'password2': 'ProAdmin888'
-            }
+def test_signup_serializer_validation():
+    data = {
+        'email': 'dmytriiii@mail.com',
+        'password': 'PassWord777',
+        'password2': 'PassWord777',
+        'first_name': 'dmytrii',
+        'last_name': 'test',
+        'birth_date': '2018-02-04'
+    }
     serializer = SignUpSerializer(data=data)
     assert serializer.is_valid()
     serializer.save()
