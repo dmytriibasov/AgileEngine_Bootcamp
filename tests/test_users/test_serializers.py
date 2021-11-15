6f321e32ddea849bf8d13d8d1cbc1c4618f79fbd
Dmytrii Basov <dimon4a2009@gmail.com>
2021-11-15 20:06:10 +0200

    nothing special

--- tests/test_users/test_serializers.py
+++ tests/test_users/test_serializers.py
@@ -1,19 +1,9 @@
 import pytest
+from rest_framework.exceptions import ValidationError
+from fixtures import user_data
 from apps.users.serializers import SignUpSerializer
 
 
-# @pytest.mark.django_db
-# def test_signup_serializer():
-#     data = {'email': 'custom@email.com',
-#             'password': 'ProAdmin888',
-#             'password2': 'ProAdmin888'
-#             }
-#     serializer = SignUpSerializer(data=data)
-#     assert serializer.is_valid()
-#     serializer.save()
-#
-#     serializer = SignUpSerializer(data=data)
-#     assert not serializer.is_valid()
-
-def test_function():
-    assert (1, 2, 3) == (1, 2, 3)
+@pytest.mark.django_db
+def test_serializer():
+    pass
